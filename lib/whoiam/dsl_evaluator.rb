d3dd334b4623d608e919e273430c62302dad9295
Uchio KONDO <udzura@pepabo.com>
2014-07-24 20:55:40 +0900

    Implement user parser(this is WIP)

--- lib/whoiam/dsl_evaluator.rb
+++ lib/whoiam/dsl_evaluator.rb
@@ -2,13 +2,32 @@ class WhoIAM::DSLEvaluator
   def initialize(&block)
     instance_eval(&block)
   end
+  attr_accessor :users, :groupes, :roles
 
   def user name, &b
+    user = WhoIAM::User.from_hash(DSL2Hash.parse(&b).merge(user_name: name))
+    self.users ||= []
+    self.users << user
   end
 
   def group name, &b
   end
 
   def role name, &b
+  end
+
+  module DSL2Hash
+    class << self
+      def parse(&dsl)
+        @container = {}
+        instance_eval(&dsl)
+        @container
+      end
+
+      def method_missing(attr_name, value)
+        @container[attr_name] ||= []
+        @container[attr_name] << value
+      end
+    end
   end
 end
--- lib/whoiam/user.rb
+++ lib/whoiam/user.rb
@@ -1,3 +1,10 @@
 class WhoIAM::User
   attr_accessor :user_name, :password, :user_policies
+
+  def self.from_hash(params)
+    user = new
+    user.user_name = params[:user_name]
+    user.password  = params[:password].first
+    user.user_policies  = params[:policy]
+  end
 end
